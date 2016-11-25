4c7a2fef9f0717439b3a840e2846b3d0419d7723
SoleneR <solene.renoud@orange.fr>
2016-11-25 09:24:57 +0100

    Commit MaJ (pageConnexion,index,logo.html)

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'vues/pageConnexion.php';
+
+
+
+?>
--- vues/pageConnexion.php
+++ vues/pageConnexion.php
@@ -1,3 +1,26 @@
-<?php
-
-?>
+<div data-role="page" id="pageconnexion">
+    <?php
+        include "vues/entetepage.html";
+    ?>
+        <div data-role="content" id="divconnexion">
+            <?php
+                include "vues/logo.html";
+            ?>
+            <div class="ui-field-contain">
+                <label for="login">Nom</label>
+                <input type="text" name="login" id="login" value=""/>
+                <label for="mdp">Mot de passe</label>
+                <input type="password" name="mdp" id="mdp" value=""/>
+            </div>
+            <div id="message">  </div>
+                <p>
+                    <a href="#" data-role="button" id="btnconnexion" data-line="true">
+                        S'identifier
+                    </a>
+                </p>
+          
+        </div><!-- /content -->
+        <?php
+            include "vues/piedpage.html";
+        ?>
+</div><!-- /page 1 -->
