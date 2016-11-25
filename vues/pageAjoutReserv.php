8b1356e3b768a5c1b822f9ca5d140475890d9a84
SoleneR <solene.renoud@orange.fr>
2016-11-25 11:50:19 +0100

    Commit MaJ2 (pageAjoutReserv.php)

--- vues/pageAjoutReserv.php
+++ vues/pageAjoutReserv.php
@@ -3,6 +3,14 @@
         include "vues/entetePage.html"; 
     ?> 
     <div data-role = "content"> 
+        <label id="ltDescr">Brève description</label>
+        <input type="text" name="ltDescr" id="ltDescr" value=""/>  
+        
+        <label id="descrComplete">Description complète</label>
+        <textarea name="descrComplete" id="descrComplete" class="required"></textarea>
+        
+        <label id="debut">Début</label>
+        <input type="date" name="debut" id="debut" value=""/>
         
    
     </div> <!-- /fin content --> 
