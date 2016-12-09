12b591b152d4d406731475fdce042f7f6b166933
SoleneR <solene.renoud@orange.fr>
2016-12-09 15:38:24 +0100

    Supprimé pageUser.php

--- vues/pageUtilisateur.php
+++
@@ -1,9 +0,0 @@
-<div data-role="page" id ="pageUtilisateur" data-theme="b"> 
-    <?php   include "vues/entetePage.html"; ?> 
-    <div data-role = "content"> 
-        
-    </div> <!-- /fin content --> 
-        <?php 
-            include "vues/piedsPage.html"; 
-        ?> 
-</div> <!-- /fin page -->
